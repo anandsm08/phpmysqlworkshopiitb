e103990b2a1669438192652da4d5a95408dad034
unknown <anandsm08@gmail.com>
2020-10-01 09:46:08 +0530

    second commit

---
+++ DAY2/Q6_triangles.php
@@ -0,0 +1,32 @@
+<html>
+ <form action= 'Q6_triangles.php' method='GET'>
+ 	<input type='text'   name='side1'><br>
+ 	<input type='text'   name='side2'><br>
+ 	<input type='text'   name='side3'><br>
+ 	<input type='submit' value='Fill in'>
+ </form>
+ </html>
+ <?php	
+
+$a = $_GET['side1'];
+$b = $_GET['side2'];
+$c = $_GET['side3'];
+
+if ($a == $b && $b == $c)
+{
+	echo "The triangle is  equilateral";
+}
+elseif ($a == $b || $b == $c || $a == $c)
+{
+	echo "The triangle is isoceles";
+}
+elseif ($a*$a + $b*$b = $c*$c || $b*$b + $c*$c =$a*$a || $a*$a + $c*$c = $b*$b)
+{
+ echo "The Triangle is right angled";
+}
+else {
+	echo "The triangle is scalene";
+}
+
+
+?>
