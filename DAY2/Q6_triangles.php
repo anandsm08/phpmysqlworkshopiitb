30365f4f8a8c8547b792c0b7b8f0bfa92d479b49
unknown <anandsm08@gmail.com>
2020-10-01 11:15:32 +0530

    second commit

--- DAY2/Q6_triangles.php
+++ DAY2/Q6_triangles.php
@@ -12,6 +12,7 @@ $a = $_GET['side1'];
 $b = $_GET['side2'];
 $c = $_GET['side3'];
 
+if ($a || $b || $c) {
 if ($a == $b && $b == $c)
 {
 	echo "The triangle is  equilateral";
@@ -20,12 +21,16 @@ elseif ($a == $b || $b == $c || $a == $c)
 {
 	echo "The triangle is isoceles";
 }
-elseif ($a*$a + $b*$b = $c*$c || $b*$b + $c*$c =$a*$a || $a*$a + $c*$c = $b*$b)
+elseif ($a*$a + $b*$b == $c*$c || $b*$b + $c*$c == $a*$a || $a*$a + $c*$c == $b*$b)
 {
  echo "The Triangle is right angled";
 }
 else {
 	echo "The triangle is scalene";
+}
+}
+else {
+	echo "Err, Put some values";
 }
 
 
